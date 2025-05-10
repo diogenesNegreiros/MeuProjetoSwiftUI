4f4c0a85bf5a7ae7dc2614613fe10517f04eef73
NEGREIROS Diogenes <diogenes.negreiros@consulting-for.edenred.com>
2025-05-09 23:00:34 -0300

    Layout perfil

--- MeuProjetoSwiftUi/ContentView.swift
+++ MeuProjetoSwiftUi/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemGreen)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("perfil")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 300)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay {
+                        Circle().stroke(Color.white, lineWidth: 4)
+                    }
+                Text("Diógenes Negreiros")
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Text("Dev IOS e Android")
+                    .font(.system(size: 16))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                RoundedRectangle(cornerRadius: 25)
+                    .frame(height: 50)
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .overlay (Text("+55 (21) 994047961"))
+
+            }
         }
-        .padding()
+        
     }
 }
 
