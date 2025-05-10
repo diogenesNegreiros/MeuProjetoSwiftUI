417b71a44ec4e164675df29db53cbc2875f4a421
NEGREIROS Diogenes <diogenes.negreiros@consulting-for.edenred.com>
2025-05-09 22:54:31 -0300

    Initial Commit

---
+++ MeuProjetoSwiftUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeuProjetoSwiftUi
+//
+//  Created by Diogenes de Souza Negreiros on 09/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeuProjetoSwiftUi/MeuProjetoSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeuProjetoSwiftUiApp.swift
+//  MeuProjetoSwiftUi
+//
+//  Created by Diogenes de Souza Negreiros on 09/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MeuProjetoSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
